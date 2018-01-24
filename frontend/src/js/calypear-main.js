52e239401798246abf260e3df5d8c0b5d2e4d347
Jay <jay@MacBook-Pro.home>
2018-01-24 23:13:23 +0000

    Starting ARch Component Functions

--- frontend/src/js/calypear-main.js
+++ frontend/src/js/calypear-main.js
@@ -1,16 +1,35 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import store from './calypear-store.js';
+import VueRouter from 'vue-router';
+import store from './store/calypear-store.js';
 import componentTypeList from './component-type-list.vue';
+import archComponent from './arch-component/arch-component.vue';
 //import store from './calypear-store.js';
 
+Vue.use(VueRouter);
 
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/component-type',
+      component: componentTypeList
+    },
+    {
+      path: '/arch-component/:id',
+      component: archComponent
+    }
+  ]
+})
 
-new Vue({
-  el: '#vueapp',
-  store,
-  components: {
-    componentTypeList
-  }
-
-});
+const app = new Vue({
+  router: router,
+  store
+}).$mount('#vueapp');
+// new Vue({
+//   el: '#vueapp',
+//   store,
+//   components: {
+//     componentTypeList
+//   }
+//
+// });
