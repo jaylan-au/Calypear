a1f2fe9d3a0f9ed181fbab9770ac35cbfe417294
Jay <jay@MacBook-Pro.home>
2018-02-27 22:32:07 +0000

    Started adding auth with passport js

--- backend/modules/calypear-base/index.js
+++ backend/modules/calypear-base/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const passport = require('passport');
+const passportLocal = require('passport-local');
 
 let pluginModule = {
   checkAndCreateAdminUser(app) {
